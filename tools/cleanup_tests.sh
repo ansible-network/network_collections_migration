41c86d130574bd0789835fa4df7c195df1dfa36c
Paul Belanger <pabelanger@redhat.com>
2019-12-14 21:25:34 -0500

    Convert httpapi connection to ansible.netcommon.httpapi
    Signed-off-by: Paul Belanger <pabelanger@redhat.com>

--- tools/cleanup_tests.sh
+++ tools/cleanup_tests.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 set -ex
 
+# httpapi -> ansible.netcommon.httpapi
+grep -RiIl 'httpapi' | xargs sed -i 's/\(connection\)\(.*\)\(httpapi\)/\1\2ansible.netcommon.\3/g' | true
 # network_cli -> ansible.netcommon.network_cli
-grep -RiIl 'network_cli' | xargs sed -i 's/\(connection\)\(.*\)\(network_cli\)/\1\2ansible.netcommon.\3/g'
+grep -RiIl 'network_cli' | xargs sed -i 's/\(connection\)\(.*\)\(network_cli\)/\1\2ansible.netcommon.\3/g' | true
