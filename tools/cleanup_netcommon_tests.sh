4fcfeeb8bbde7e21af8f1744a2001076828951a5
Paul Belanger <pabelanger@redhat.com>
2020-01-21 12:13:35 -0500

    Support junipernetworks.junos in tools/cleanup_netcommon_tests.sh
    Signed-off-by: Paul Belanger <pabelanger@redhat.com>

--- tools/cleanup_netcommon_tests.sh
+++ tools/cleanup_netcommon_tests.sh
@@ -3,3 +3,6 @@ set -ex
 
 # iosxr -> cisco.iosxr.iosxr
 grep -RiIl 'ansible_network_os' | xargs perl -i -pe "s/'iosxr'/'cisco.iosxr.iosxr'/g"
+
+# junos -> junipernetworks.junos.junos
+grep -RiIl 'ansible_network_os' | xargs perl -i -pe "s/'junos'/'junipernetworks.junos.junos'/g"
